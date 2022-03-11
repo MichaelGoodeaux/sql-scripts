94dd6ab292777175cea891ba57b55f7914e4ee81
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 12:41:09 -0600

    Nomenclature update

--- SelectRowCountsALLTABLES-var4.sql
+++ SelectRowCountsALLTABLES-var4.sql
@@ -1,3 +1,6 @@
+#One of 4 of my SQL scripts that I use in specific situations to get row counts for all tables across all databases. 
+#This helps when comparing this information across migratory processes.
+
 DECLARE @QueryString NVARCHAR(MAX) ;
 SELECT @QueryString = COALESCE(@QueryString + ' UNION ALL ','')
                       + 'SELECT '
