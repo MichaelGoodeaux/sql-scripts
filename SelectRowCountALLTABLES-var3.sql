e6d095cde894b28135f6f4b8e7147ab7473a4d41
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 12:44:54 -0600

    Name update

--- SelectRowCountALLTABLES-var3.sql
+++ SelectRowCountALLTABLES-var3.sql
@@ -1,3 +1,6 @@
+-- One of 4 of my SQL scripts that I use in specific situations to get row counts for all tables across all databases. 
+-- This helps when comparing this information across migratory processes.
+
 DECLARE @TableRowCounts TABLE ([TableName] VARCHAR(128), [RowCount] BIGINT) ;
 INSERT INTO @TableRowCounts ([TableName], [RowCount])
 EXEC sp_MSforeachtable 'SELECT ''?'' [TableName], COUNT(*) [RowCount] FROM ?' ;
