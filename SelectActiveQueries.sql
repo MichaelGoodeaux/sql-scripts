d9c807fd432b056eb8f006a2b6c43e3109d2bc85
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 12:44:06 -0600

    Update SelectActiveQueries.sql

--- SelectActiveQueries.sql
+++ SelectActiveQueries.sql
@@ -1,5 +1,5 @@
-#On a regular basis, I need to see which queries are running as often times I need to kill something stuck. 
-#While there are multiple ways to go about this, I frankensteined this query together in order to see which queries were long running and hogging resources
+-- On a regular basis, I need to see which queries are running as often times I need to kill something stuck. 
+-- While there are multiple ways to go about this, I frankensteined this query together in order to see which queries were long running and hogging resources
 
 SELECT sqltext.TEXT,
 req.session_id,
