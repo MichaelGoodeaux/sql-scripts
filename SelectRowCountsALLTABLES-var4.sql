6623ff7f88eb2bc7459d6a2daf448c93a8665b36
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 12:43:45 -0600

    Update SelectRowCountsALLTABLES-var4.sql

--- SelectRowCountsALLTABLES-var4.sql
+++ SelectRowCountsALLTABLES-var4.sql
@@ -1,5 +1,5 @@
-#One of 4 of my SQL scripts that I use in specific situations to get row counts for all tables across all databases. 
-#This helps when comparing this information across migratory processes.
+-- One of 4 of my SQL scripts that I use in specific situations to get row counts for all tables across all databases. 
+-- This helps when comparing this information across migratory processes.
 
 DECLARE @QueryString NVARCHAR(MAX) ;
 SELECT @QueryString = COALESCE(@QueryString + ' UNION ALL ','')
