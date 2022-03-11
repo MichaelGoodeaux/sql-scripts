5cbf80e2d967a7100230f3893bdc7f7b2352e70f
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 13:47:28 -0500

    Add files via upload

---
+++ FindColumnbyTableinDatabase.sql
@@ -0,0 +1,9 @@
+USE %database%
+GO
+SELECT t.name AS table_name,
+SCHEMA_NAME(schema_id) AS schema_name,
+c.name AS column_name
+FROM sys.tables AS t
+INNER JOIN sys.columns c ON t.OBJECT_ID = c.OBJECT_ID
+WHERE c.name LIKE '%columnname%'
+ORDER BY schema_name, table_name;
