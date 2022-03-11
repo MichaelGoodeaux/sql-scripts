bb93ab4cc795099c1c3b4b86f47eb4eb03b47269
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 12:49:10 -0600

    Context notes

--- FindColumnbyTableinDatabase.sql
+++ FindColumnbyTableinDatabase.sql
@@ -1,3 +1,7 @@
+-- There were several cases of needing to find a specific column across a large tableset
+-- This utility script helps me quickly check for which tables contain a specific column
+-- Left variables in place for customization as needed for situation
+
 USE %database%
 GO
 SELECT t.name AS table_name,
